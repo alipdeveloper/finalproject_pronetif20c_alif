ab482af73567f2d4df1edb782e01a63db3b5f443
Hanip20 <khanifprasetyo90@gmail.com>
2022-12-28 13:16:06 +0700

    Tambah Suplai

--- Vape Store/Form3.Designer.cs
+++ Vape Store/Form3.Designer.cs
@@ -30,21 +30,22 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
-            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
-            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton6 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton7 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
-            this.toolStripButton8 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton9 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton10 = new System.Windows.Forms.ToolStripButton();
             this.tdMaster = new System.Windows.Forms.ToolStripDropDownButton();
             this.tdTransaksi = new System.Windows.Forms.ToolStripDropDownButton();
             this.tdProgram = new System.Windows.Forms.ToolStripDropDownButton();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.tsBarang = new System.Windows.Forms.ToolStripButton();
+            this.tsKategori = new System.Windows.Forms.ToolStripButton();
+            this.tsSatuan = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsPenjualan = new System.Windows.Forms.ToolStripButton();
+            this.tsLaporan = new System.Windows.Forms.ToolStripButton();
+            this.tsKas = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsPassword = new System.Windows.Forms.ToolStripButton();
+            this.tsKalkulator = new System.Windows.Forms.ToolStripButton();
+            this.tsSetting = new System.Windows.Forms.ToolStripButton();
+            this.tsSuplai = new System.Windows.Forms.ToolStripButton();
             this.toolStrip1.SuspendLayout();
             this.toolStrip2.SuspendLayout();
             this.SuspendLayout();
@@ -62,110 +63,6 @@
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
-            // 
-            // toolStrip2
-            // 
-            this.toolStrip2.ImageScalingSize = new System.Drawing.Size(24, 24);
-            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripButton1,
-            this.toolStripButton2,
-            this.toolStripButton3,
-            this.toolStripSeparator3,
-            this.toolStripButton5,
-            this.toolStripButton6,
-            this.toolStripButton7,
-            this.toolStripSeparator4,
-            this.toolStripButton8,
-            this.toolStripButton9,
-            this.toolStripButton10});
-            this.toolStrip2.Location = new System.Drawing.Point(0, 34);
-            this.toolStrip2.Name = "toolStrip2";
-            this.toolStrip2.Size = new System.Drawing.Size(1056, 34);
-            this.toolStrip2.TabIndex = 1;
-            this.toolStrip2.Text = "toolStrip2";
-            // 
-            // toolStripButton1
-            // 
-            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
-            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(95, 29);
-            this.toolStripButton1.Text = "Barang";
-            // 
-            // toolStripButton2
-            // 
-            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
-            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton2.Name = "toolStripButton2";
-            this.toolStripButton2.Size = new System.Drawing.Size(106, 29);
-            this.toolStripButton2.Text = "Kategori";
-            // 
-            // toolStripButton3
-            // 
-            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
-            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton3.Name = "toolStripButton3";
-            this.toolStripButton3.Size = new System.Drawing.Size(94, 29);
-            this.toolStripButton3.Text = "Satuan";
-            // 
-            // toolStripSeparator3
-            // 
-            this.toolStripSeparator3.Name = "toolStripSeparator3";
-            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 34);
-            // 
-            // toolStripButton5
-            // 
-            this.toolStripButton5.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton5.Image")));
-            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton5.Name = "toolStripButton5";
-            this.toolStripButton5.Size = new System.Drawing.Size(114, 29);
-            this.toolStripButton5.Text = "Penjualan";
-            this.toolStripButton5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            // 
-            // toolStripButton6
-            // 
-            this.toolStripButton6.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton6.Image")));
-            this.toolStripButton6.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton6.Name = "toolStripButton6";
-            this.toolStripButton6.Size = new System.Drawing.Size(104, 29);
-            this.toolStripButton6.Text = "Laporan";
-            // 
-            // toolStripButton7
-            // 
-            this.toolStripButton7.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton7.Image")));
-            this.toolStripButton7.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton7.Name = "toolStripButton7";
-            this.toolStripButton7.Size = new System.Drawing.Size(67, 29);
-            this.toolStripButton7.Text = "Kas";
-            // 
-            // toolStripSeparator4
-            // 
-            this.toolStripSeparator4.Name = "toolStripSeparator4";
-            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 34);
-            // 
-            // toolStripButton8
-            // 
-            this.toolStripButton8.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton8.Image")));
-            this.toolStripButton8.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton8.Name = "toolStripButton8";
-            this.toolStripButton8.Size = new System.Drawing.Size(115, 29);
-            this.toolStripButton8.Text = "Password";
-            // 
-            // toolStripButton9
-            // 
-            this.toolStripButton9.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton9.Image")));
-            this.toolStripButton9.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton9.Name = "toolStripButton9";
-            this.toolStripButton9.Size = new System.Drawing.Size(118, 29);
-            this.toolStripButton9.Text = "Kalkulator";
-            // 
-            // toolStripButton10
-            // 
-            this.toolStripButton10.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton10.Image")));
-            this.toolStripButton10.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton10.Name = "toolStripButton10";
-            this.toolStripButton10.Size = new System.Drawing.Size(96, 29);
-            this.toolStripButton10.Text = "Setting";
             // 
             // tdMaster
             // 
@@ -194,6 +91,119 @@
             this.tdProgram.Size = new System.Drawing.Size(99, 29);
             this.tdProgram.Text = "Program";
             // 
+            // toolStrip2
+            // 
+            this.toolStrip2.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsSuplai,
+            this.tsBarang,
+            this.tsKategori,
+            this.tsSatuan,
+            this.toolStripSeparator3,
+            this.tsPenjualan,
+            this.tsLaporan,
+            this.tsKas,
+            this.toolStripSeparator4,
+            this.tsPassword,
+            this.tsKalkulator,
+            this.tsSetting});
+            this.toolStrip2.Location = new System.Drawing.Point(0, 34);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(1056, 34);
+            this.toolStrip2.TabIndex = 1;
+            this.toolStrip2.Text = "toolStrip2";
+            // 
+            // tsBarang
+            // 
+            this.tsBarang.Image = ((System.Drawing.Image)(resources.GetObject("tsBarang.Image")));
+            this.tsBarang.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsBarang.Name = "tsBarang";
+            this.tsBarang.Size = new System.Drawing.Size(95, 29);
+            this.tsBarang.Text = "Barang";
+            // 
+            // tsKategori
+            // 
+            this.tsKategori.Image = ((System.Drawing.Image)(resources.GetObject("tsKategori.Image")));
+            this.tsKategori.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsKategori.Name = "tsKategori";
+            this.tsKategori.Size = new System.Drawing.Size(106, 29);
+            this.tsKategori.Text = "Kategori";
+            // 
+            // tsSatuan
+            // 
+            this.tsSatuan.Image = ((System.Drawing.Image)(resources.GetObject("tsSatuan.Image")));
+            this.tsSatuan.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsSatuan.Name = "tsSatuan";
+            this.tsSatuan.Size = new System.Drawing.Size(94, 29);
+            this.tsSatuan.Text = "Satuan";
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 34);
+            // 
+            // tsPenjualan
+            // 
+            this.tsPenjualan.Image = ((System.Drawing.Image)(resources.GetObject("tsPenjualan.Image")));
+            this.tsPenjualan.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsPenjualan.Name = "tsPenjualan";
+            this.tsPenjualan.Size = new System.Drawing.Size(114, 29);
+            this.tsPenjualan.Text = "Penjualan";
+            this.tsPenjualan.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // tsLaporan
+            // 
+            this.tsLaporan.Image = ((System.Drawing.Image)(resources.GetObject("tsLaporan.Image")));
+            this.tsLaporan.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsLaporan.Name = "tsLaporan";
+            this.tsLaporan.Size = new System.Drawing.Size(104, 29);
+            this.tsLaporan.Text = "Laporan";
+            // 
+            // tsKas
+            // 
+            this.tsKas.Image = ((System.Drawing.Image)(resources.GetObject("tsKas.Image")));
+            this.tsKas.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsKas.Name = "tsKas";
+            this.tsKas.Size = new System.Drawing.Size(67, 29);
+            this.tsKas.Text = "Kas";
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 34);
+            // 
+            // tsPassword
+            // 
+            this.tsPassword.Image = ((System.Drawing.Image)(resources.GetObject("tsPassword.Image")));
+            this.tsPassword.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsPassword.Name = "tsPassword";
+            this.tsPassword.Size = new System.Drawing.Size(115, 29);
+            this.tsPassword.Text = "Password";
+            // 
+            // tsKalkulator
+            // 
+            this.tsKalkulator.Image = ((System.Drawing.Image)(resources.GetObject("tsKalkulator.Image")));
+            this.tsKalkulator.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsKalkulator.Name = "tsKalkulator";
+            this.tsKalkulator.Size = new System.Drawing.Size(118, 29);
+            this.tsKalkulator.Text = "Kalkulator";
+            // 
+            // tsSetting
+            // 
+            this.tsSetting.Image = ((System.Drawing.Image)(resources.GetObject("tsSetting.Image")));
+            this.tsSetting.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsSetting.Name = "tsSetting";
+            this.tsSetting.Size = new System.Drawing.Size(96, 29);
+            this.tsSetting.Text = "Setting";
+            // 
+            // tsSuplai
+            // 
+            this.tsSuplai.Image = ((System.Drawing.Image)(resources.GetObject("tsSuplai.Image")));
+            this.tsSuplai.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsSuplai.Name = "tsSuplai";
+            this.tsSuplai.Size = new System.Drawing.Size(88, 29);
+            this.tsSuplai.Text = "Suplai";
+            // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
@@ -219,16 +229,17 @@
         private ToolStripDropDownButton tdTransaksi;
         private ToolStripDropDownButton tdProgram;
         private ToolStrip toolStrip2;
-        private ToolStripButton toolStripButton1;
-        private ToolStripButton toolStripButton2;
-        private ToolStripButton toolStripButton3;
+        private ToolStripButton tsBarang;
+        private ToolStripButton tsKategori;
+        private ToolStripButton tsSatuan;
         private ToolStripSeparator toolStripSeparator3;
-        private ToolStripButton toolStripButton5;
-        private ToolStripButton toolStripButton6;
-        private ToolStripButton toolStripButton7;
+        private ToolStripButton tsPenjualan;
+        private ToolStripButton tsLaporan;
+        private ToolStripButton tsKas;
         private ToolStripSeparator toolStripSeparator4;
-        private ToolStripButton toolStripButton8;
-        private ToolStripButton toolStripButton9;
-        private ToolStripButton toolStripButton10;
+        private ToolStripButton tsPassword;
+        private ToolStripButton tsKalkulator;
+        private ToolStripButton tsSetting;
+        private ToolStripButton tsSuplai;
     }
 }
