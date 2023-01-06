af6dbce31264f387c03010d14fbd337870091aed
Sygit24 <sygit2222@gmail.com>
2023-01-06 15:19:32 +0700

    Tambah FrmPengaturan dan FrmTambahUser

---
+++ KasirVape/FrmPengaturan.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace KasirVape
+{
+    partial class FrmPengaturan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btnKeluar = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnUbah = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnKeluar
+            // 
+            this.btnKeluar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnKeluar.AutoSize = true;
+            this.btnKeluar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnKeluar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKeluar.Location = new System.Drawing.Point(223, 194);
+            this.btnKeluar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnKeluar.Name = "btnKeluar";
+            this.btnKeluar.Size = new System.Drawing.Size(60, 28);
+            this.btnKeluar.TabIndex = 23;
+            this.btnKeluar.Text = "Keluar";
+            this.btnKeluar.UseVisualStyleBackColor = true;
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnHapus.AutoSize = true;
+            this.btnHapus.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnHapus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHapus.Location = new System.Drawing.Point(154, 194);
+            this.btnHapus.Margin = new System.Windows.Forms.Padding(4);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(61, 28);
+            this.btnHapus.TabIndex = 22;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnTambah.AutoSize = true;
+            this.btnTambah.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnTambah.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTambah.Location = new System.Drawing.Point(13, 194);
+            this.btnTambah.Margin = new System.Windows.Forms.Padding(4);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(72, 28);
+            this.btnTambah.TabIndex = 20;
+            this.btnTambah.Text = "Tambah";
+            this.btnTambah.UseVisualStyleBackColor = true;
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "FrmSet";
+            this.Column10.Name = "Column10";
+            this.Column10.ReadOnly = true;
+            this.Column10.Width = 82;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "FrmPass";
+            this.Column9.Name = "Column9";
+            this.Column9.ReadOnly = true;
+            this.Column9.Width = 94;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "FrmLap";
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            this.Column8.Width = 84;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "FrmJual";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            this.Column7.Width = 87;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "FrmSat";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 82;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "FrmKat";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 82;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "FrmBrg";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 83;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Aktif";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 61;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Pass";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 67;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "User";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 65;
+            // 
+            // btnUbah
+            // 
+            this.btnUbah.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnUbah.AutoSize = true;
+            this.btnUbah.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnUbah.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUbah.Location = new System.Drawing.Point(93, 194);
+            this.btnUbah.Margin = new System.Windows.Forms.Padding(4);
+            this.btnUbah.Name = "btnUbah";
+            this.btnUbah.Size = new System.Drawing.Size(53, 28);
+            this.btnUbah.TabIndex = 21;
+            this.btnUbah.Text = "Ubah";
+            this.btnUbah.UseVisualStyleBackColor = true;
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgv.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgv.Location = new System.Drawing.Point(13, 13);
+            this.dgv.Margin = new System.Windows.Forms.Padding(4);
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.RowHeadersVisible = false;
+            this.dgv.Size = new System.Drawing.Size(802, 173);
+            this.dgv.TabIndex = 19;
+            // 
+            // FrmPengaturan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(828, 231);
+            this.Controls.Add(this.btnKeluar);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnTambah);
+            this.Controls.Add(this.btnUbah);
+            this.Controls.Add(this.dgv);
+            this.Name = "FrmPengaturan";
+            this.Text = "FrmPengaturan";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKeluar;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnTambah;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.Button btnUbah;
+        private System.Windows.Forms.DataGridView dgv;
+
+    }
+}
---
+++ KasirVape/FrmTambahUser.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace KasirVape
+{
+    partial class FrmTambahUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ckFrmKas = new System.Windows.Forms.CheckBox();
+            this.ckAktif = new System.Windows.Forms.CheckBox();
+            this.ckFrmSet = new System.Windows.Forms.CheckBox();
+            this.ckFrmPass = new System.Windows.Forms.CheckBox();
+            this.ckFrmLap = new System.Windows.Forms.CheckBox();
+            this.ckFrmJual = new System.Windows.Forms.CheckBox();
+            this.ckFrmSat = new System.Windows.Forms.CheckBox();
+            this.ckFrmKat = new System.Windows.Forms.CheckBox();
+            this.ckFrmBrg = new System.Windows.Forms.CheckBox();
+            this.btnKeluar = new System.Windows.Forms.Button();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ckFrmKas
+            // 
+            this.ckFrmKas.AutoSize = true;
+            this.ckFrmKas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckFrmKas.Location = new System.Drawing.Point(224, 174);
+            this.ckFrmKas.Margin = new System.Windows.Forms.Padding(4);
+            this.ckFrmKas.Name = "ckFrmKas";
+            this.ckFrmKas.Size = new System.Drawing.Size(96, 22);
+            this.ckFrmKas.TabIndex = 62;
+            this.ckFrmKas.Text = "Form Kas";
+            this.ckFrmKas.UseVisualStyleBackColor = true;
+            // 
+            // ckAktif
+            // 
+            this.ckAktif.AutoSize = true;
+            this.ckAktif.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckAktif.Location = new System.Drawing.Point(126, 105);
+            this.ckAktif.Margin = new System.Windows.Forms.Padding(4);
+            this.ckAktif.Name = "ckAktif";
+            this.ckAktif.Size = new System.Drawing.Size(58, 22);
+            this.ckAktif.TabIndex = 61;
+            this.ckAktif.Text = "Aktif";
+            this.ckAktif.UseVisualStyleBackColor = true;
+            // 
+            // ckFrmSet
+            // 
+            this.ckFrmSet.AutoSize = true;
+            this.ckFrmSet.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckFrmSet.Location = new System.Drawing.Point(224, 231);
+            this.ckFrmSet.Margin = new System.Windows.Forms.Padding(4);
+            this.ckFrmSet.Name = "ckFrmSet";
+            this.ckFrmSet.Size = new System.Drawing.Size(115, 22);
+            this.ckFrmSet.TabIndex = 60;
+            this.ckFrmSet.Text = "Form Setting";
+            this.ckFrmSet.UseVisualStyleBackColor = true;
+            // 
+            // ckFrmPass
+            // 
+            this.ckFrmPass.AutoSize = true;
+            this.ckFrmPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckFrmPass.Location = new System.Drawing.Point(224, 202);
+            this.ckFrmPass.Margin = new System.Windows.Forms.Padding(4);
+            this.ckFrmPass.Name = "ckFrmPass";
+            this.ckFrmPass.Size = new System.Drawing.Size(137, 22);
+            this.ckFrmPass.TabIndex = 59;
+            this.ckFrmPass.Text = "Form Password";
+            this.ckFrmPass.UseVisualStyleBackColor = true;
+            // 
+            // ckFrmLap
+            // 
+            this.ckFrmLap.AutoSize = true;
+            this.ckFrmLap.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckFrmLap.Location = new System.Drawing.Point(224, 146);
+            this.ckFrmLap.Margin = new System.Windows.Forms.Padding(4);
+            this.ckFrmLap.Name = "ckFrmLap";
+            this.ckFrmLap.Size = new System.Drawing.Size(124, 22);
+            this.ckFrmLap.TabIndex = 58;
+            this.ckFrmLap.Text = "Form Laporan";
+            this.ckFrmLap.UseVisualStyleBackColor = true;
+            // 
+            // ckFrmJual
+            // 
+            this.ckFrmJual.AutoSize = true;
+            this.ckFrmJual.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckFrmJual.Location = new System.Drawing.Point(23, 231);
+            this.ckFrmJual.Margin = new System.Windows.Forms.Padding(4);
+            this.ckFrmJual.Name = "ckFrmJual";
+            this.ckFrmJual.Size = new System.Drawing.Size(134, 22);
+            this.ckFrmJual.TabIndex = 57;
+            this.ckFrmJual.Text = "Form Penjualan";
+            this.ckFrmJual.UseVisualStyleBackColor = true;
+            // 
+            // ckFrmSat
+            // 
+            this.ckFrmSat.AutoSize = true;
+            this.ckFrmSat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckFrmSat.Location = new System.Drawing.Point(23, 202);
+            this.ckFrmSat.Margin = new System.Windows.Forms.Padding(4);
+            this.ckFrmSat.Name = "ckFrmSat";
+            this.ckFrmSat.Size = new System.Drawing.Size(116, 22);
+            this.ckFrmSat.TabIndex = 56;
+            this.ckFrmSat.Text = "Form Satuan";
+            this.ckFrmSat.UseVisualStyleBackColor = true;
+            // 
+            // ckFrmKat
+            // 
+            this.ckFrmKat.AutoSize = true;
+            this.ckFrmKat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckFrmKat.Location = new System.Drawing.Point(23, 174);
+            this.ckFrmKat.Margin = new System.Windows.Forms.Padding(4);
+            this.ckFrmKat.Name = "ckFrmKat";
+            this.ckFrmKat.Size = new System.Drawing.Size(125, 22);
+            this.ckFrmKat.TabIndex = 55;
+            this.ckFrmKat.Text = "Form Kategori";
+            this.ckFrmKat.UseVisualStyleBackColor = true;
+            // 
+            // ckFrmBrg
+            // 
+            this.ckFrmBrg.AutoSize = true;
+            this.ckFrmBrg.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckFrmBrg.Location = new System.Drawing.Point(23, 146);
+            this.ckFrmBrg.Margin = new System.Windows.Forms.Padding(4);
+            this.ckFrmBrg.Name = "ckFrmBrg";
+            this.ckFrmBrg.Size = new System.Drawing.Size(117, 22);
+            this.ckFrmBrg.TabIndex = 54;
+            this.ckFrmBrg.Text = "Form Barang";
+            this.ckFrmBrg.UseVisualStyleBackColor = true;
+            // 
+            // btnKeluar
+            // 
+            this.btnKeluar.AutoSize = true;
+            this.btnKeluar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnKeluar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKeluar.Location = new System.Drawing.Point(246, 261);
+            this.btnKeluar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnKeluar.Name = "btnKeluar";
+            this.btnKeluar.Size = new System.Drawing.Size(105, 28);
+            this.btnKeluar.TabIndex = 53;
+            this.btnKeluar.Text = "Keluar (ESC)";
+            this.btnKeluar.UseVisualStyleBackColor = true;
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.AutoSize = true;
+            this.btnSimpan.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnSimpan.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSimpan.Location = new System.Drawing.Point(23, 261);
+            this.btnSimpan.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(99, 28);
+            this.btnSimpan.TabIndex = 52;
+            this.btnSimpan.Text = "Simpan (F3)";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            // 
+            // txtPass
+            // 
+            this.txtPass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass.Location = new System.Drawing.Point(126, 72);
+            this.txtPass.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(161, 24);
+            this.txtPass.TabIndex = 51;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(19, 75);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 18);
+            this.label2.TabIndex = 50;
+            this.label2.Text = "Pass";
+            // 
+            // txtUser
+            // 
+            this.txtUser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUser.Location = new System.Drawing.Point(126, 40);
+            this.txtUser.Margin = new System.Windows.Forms.Padding(4);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(161, 24);
+            this.txtUser.TabIndex = 49;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 43);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 18);
+            this.label1.TabIndex = 48;
+            this.label1.Text = "User";
+            // 
+            // FrmTambahUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(381, 328);
+            this.Controls.Add(this.ckFrmKas);
+            this.Controls.Add(this.ckAktif);
+            this.Controls.Add(this.ckFrmSet);
+            this.Controls.Add(this.ckFrmPass);
+            this.Controls.Add(this.ckFrmLap);
+            this.Controls.Add(this.ckFrmJual);
+            this.Controls.Add(this.ckFrmSat);
+            this.Controls.Add(this.ckFrmKat);
+            this.Controls.Add(this.ckFrmBrg);
+            this.Controls.Add(this.btnKeluar);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "FrmTambahUser";
+            this.Text = "FrmTambahUser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox ckFrmKas;
+        private System.Windows.Forms.CheckBox ckAktif;
+        private System.Windows.Forms.CheckBox ckFrmSet;
+        private System.Windows.Forms.CheckBox ckFrmPass;
+        private System.Windows.Forms.CheckBox ckFrmLap;
+        private System.Windows.Forms.CheckBox ckFrmJual;
+        private System.Windows.Forms.CheckBox ckFrmSat;
+        private System.Windows.Forms.CheckBox ckFrmKat;
+        private System.Windows.Forms.CheckBox ckFrmBrg;
+        private System.Windows.Forms.Button btnKeluar;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label1;
+    }
+}
