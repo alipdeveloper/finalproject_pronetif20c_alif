ba426cf010eeab65d8420ca4bcd1e8e0b0983b5f
Mochamad Alif Prayogo <105422250+alipdeveloper@users.noreply.github.com>
2023-01-16 03:06:10 +0700

    Frm KasTambah

---
+++ KasirVape/FrmKasTambah.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace KasirVape
+{
+    partial class FrmKasTambah
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFaktur = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtKeterangan = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPemasukan = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPengeluaran = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtJenis = new System.Windows.Forms.ComboBox();
+            this.txtKategori = new System.Windows.Forms.ComboBox();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.btnKeluar = new System.Windows.Forms.Button();
+            this.btnAuto = new System.Windows.Forms.Button();
+            this.btnCariKategoriKas = new System.Windows.Forms.Button();
+            this.txtTanggal = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(20, 52);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Faktur";
+            // 
+            // txtFaktur
+            // 
+            this.txtFaktur.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtFaktur.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFaktur.Location = new System.Drawing.Point(127, 48);
+            this.txtFaktur.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtFaktur.Name = "txtFaktur";
+            this.txtFaktur.Size = new System.Drawing.Size(197, 24);
+            this.txtFaktur.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(20, 85);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Jenis";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(20, 209);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Pengluaran";
+            // 
+            // txtKeterangan
+            // 
+            this.txtKeterangan.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtKeterangan.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKeterangan.Location = new System.Drawing.Point(127, 117);
+            this.txtKeterangan.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtKeterangan.Multiline = true;
+            this.txtKeterangan.Name = "txtKeterangan";
+            this.txtKeterangan.Size = new System.Drawing.Size(353, 50);
+            this.txtKeterangan.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(20, 121);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 18);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Keterangan";
+            // 
+            // txtPemasukan
+            // 
+            this.txtPemasukan.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPemasukan.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPemasukan.Location = new System.Drawing.Point(127, 175);
+            this.txtPemasukan.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPemasukan.Name = "txtPemasukan";
+            this.txtPemasukan.Size = new System.Drawing.Size(161, 24);
+            this.txtPemasukan.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(20, 178);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 18);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Pemasukan";
+            // 
+            // txtPengeluaran
+            // 
+            this.txtPengeluaran.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPengeluaran.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPengeluaran.Location = new System.Drawing.Point(127, 207);
+            this.txtPengeluaran.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPengeluaran.Name = "txtPengeluaran";
+            this.txtPengeluaran.Size = new System.Drawing.Size(161, 24);
+            this.txtPengeluaran.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(20, 21);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 18);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Tanggal";
+            // 
+            // txtJenis
+            // 
+            this.txtJenis.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtJenis.FormattingEnabled = true;
+            this.txtJenis.Location = new System.Drawing.Point(127, 81);
+            this.txtJenis.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtJenis.Name = "txtJenis";
+            this.txtJenis.Size = new System.Drawing.Size(160, 26);
+            this.txtJenis.TabIndex = 14;
+            this.txtJenis.SelectedIndexChanged += new System.EventHandler(this.txtJenis_SelectedIndexChanged);
+            // 
+            // txtKategori
+            // 
+            this.txtKategori.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKategori.FormattingEnabled = true;
+            this.txtKategori.Location = new System.Drawing.Point(296, 81);
+            this.txtKategori.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtKategori.Name = "txtKategori";
+            this.txtKategori.Size = new System.Drawing.Size(183, 26);
+            this.txtKategori.TabIndex = 15;
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.AutoSize = true;
+            this.btnSimpan.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnSimpan.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSimpan.ForeColor = System.Drawing.Color.Black;
+            this.btnSimpan.Location = new System.Drawing.Point(24, 255);
+            this.btnSimpan.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(99, 28);
+            this.btnSimpan.TabIndex = 11;
+            this.btnSimpan.Text = "Simpan (F3)";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            this.btnSimpan.Click += new System.EventHandler(this.btnSimpan_Click);
+            // 
+            // btnKeluar
+            // 
+            this.btnKeluar.AutoSize = true;
+            this.btnKeluar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnKeluar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKeluar.ForeColor = System.Drawing.Color.Black;
+            this.btnKeluar.Location = new System.Drawing.Point(423, 255);
+            this.btnKeluar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnKeluar.Name = "btnKeluar";
+            this.btnKeluar.Size = new System.Drawing.Size(105, 28);
+            this.btnKeluar.TabIndex = 12;
+            this.btnKeluar.Text = "Keluar (ESC)";
+            this.btnKeluar.UseVisualStyleBackColor = true;
+            this.btnKeluar.Click += new System.EventHandler(this.btnKeluar_Click);
+            // 
+            // btnAuto
+            // 
+            this.btnAuto.AutoSize = true;
+            this.btnAuto.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnAuto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAuto.ForeColor = System.Drawing.Color.Black;
+            this.btnAuto.Location = new System.Drawing.Point(332, 46);
+            this.btnAuto.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAuto.Name = "btnAuto";
+            this.btnAuto.Size = new System.Drawing.Size(48, 28);
+            this.btnAuto.TabIndex = 1;
+            this.btnAuto.Text = "Auto";
+            this.btnAuto.UseVisualStyleBackColor = true;
+            this.btnAuto.Click += new System.EventHandler(this.txtAuto_Click);
+            // 
+            // btnCariKategoriKas
+            // 
+            this.btnCariKategoriKas.AutoSize = true;
+            this.btnCariKategoriKas.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnCariKategoriKas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCariKategoriKas.ForeColor = System.Drawing.Color.Black;
+            this.btnCariKategoriKas.Location = new System.Drawing.Point(488, 79);
+            this.btnCariKategoriKas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCariKategoriKas.Name = "btnCariKategoriKas";
+            this.btnCariKategoriKas.Size = new System.Drawing.Size(45, 28);
+            this.btnCariKategoriKas.TabIndex = 4;
+            this.btnCariKategoriKas.Text = "Cari";
+            this.btnCariKategoriKas.UseVisualStyleBackColor = true;
+            this.btnCariKategoriKas.Click += new System.EventHandler(this.txtCariKat_Click);
+            // 
+            // txtTanggal
+            // 
+            this.txtTanggal.CustomFormat = "dd-MMM-yyyy";
+            this.txtTanggal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTanggal.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.txtTanggal.Location = new System.Drawing.Point(127, 15);
+            this.txtTanggal.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtTanggal.Name = "txtTanggal";
+            this.txtTanggal.Size = new System.Drawing.Size(157, 24);
+            this.txtTanggal.TabIndex = 21;
+            // 
+            // FrmKasTambah
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(564, 306);
+            this.Controls.Add(this.txtTanggal);
+            this.Controls.Add(this.btnCariKategoriKas);
+            this.Controls.Add(this.btnAuto);
+            this.Controls.Add(this.btnKeluar);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.txtKategori);
+            this.Controls.Add(this.txtJenis);
+            this.Controls.Add(this.txtPengeluaran);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtPemasukan);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtKeterangan);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtFaktur);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmKasTambah";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Input Data";
+            this.Load += new System.EventHandler(this.FrmTmbhBrg_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FrmTmbhBrg_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFaktur;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtKeterangan;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPemasukan;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPengeluaran;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox txtJenis;
+        private System.Windows.Forms.ComboBox txtKategori;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.Button btnKeluar;
+        private System.Windows.Forms.Button btnAuto;
+        private System.Windows.Forms.Button btnCariKategoriKas;
+        private System.Windows.Forms.DateTimePicker txtTanggal;
+    }
+}
