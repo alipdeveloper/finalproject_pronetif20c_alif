bb3515aa52ecd5b4c7aa86fbcf8e0889275c382a
Ahmadbahar911 <Ab25032001@gmail.com>
2023-01-16 17:03:20 +0700

    report nota, kas dan laporan

---
+++ KasirVape/FrmCetak.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KasirVape
+{
+    public partial class FrmCetak : Form
+    {
+        public FrmCetak()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCetak_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
