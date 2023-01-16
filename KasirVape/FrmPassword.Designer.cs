87614bdce13b4c69dc9c00442a0a69a9ea8eae9d
Ahmadbahar911 <Ab25032001@gmail.com>
2023-01-16 20:46:38 +0700

    solve password error

--- KasirVape/FrmPassword.Designer.cs
+++ KasirVape/FrmPassword.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button2 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.txtPass3 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
@@ -38,77 +37,65 @@
             this.label1 = new System.Windows.Forms.Label();
             this.txtNama = new System.Windows.Forms.ComboBox();
             this.txtPass1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button2
-            // 
-            this.button2.AutoSize = true;
-            this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(288, 183);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(51, 28);
-            this.button2.TabIndex = 23;
-            this.button2.Text = "Batal";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.SystemColors.Control;
-            this.label4.Location = new System.Drawing.Point(39, 144);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(28, 128);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(110, 18);
-            this.label4.TabIndex = 27;
+            this.label4.TabIndex = 17;
             this.label4.Text = "Password Baru";
             // 
             // txtPass3
             // 
             this.txtPass3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtPass3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPass3.Location = new System.Drawing.Point(178, 140);
-            this.txtPass3.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPass3.Location = new System.Drawing.Point(167, 124);
+            this.txtPass3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPass3.Name = "txtPass3";
             this.txtPass3.PasswordChar = '*';
             this.txtPass3.Size = new System.Drawing.Size(161, 24);
-            this.txtPass3.TabIndex = 21;
+            this.txtPass3.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.SystemColors.Control;
-            this.label3.Location = new System.Drawing.Point(39, 112);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(28, 96);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(110, 18);
-            this.label3.TabIndex = 26;
+            this.label3.TabIndex = 15;
             this.label3.Text = "Password Baru";
             // 
             // txtPass2
             // 
             this.txtPass2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtPass2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPass2.Location = new System.Drawing.Point(178, 108);
-            this.txtPass2.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPass2.Location = new System.Drawing.Point(167, 92);
+            this.txtPass2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPass2.Name = "txtPass2";
             this.txtPass2.PasswordChar = '*';
             this.txtPass2.Size = new System.Drawing.Size(161, 24);
-            this.txtPass2.TabIndex = 20;
+            this.txtPass2.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.Control;
-            this.label2.Location = new System.Drawing.Point(39, 80);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(28, 64);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(116, 18);
-            this.label2.TabIndex = 25;
+            this.label2.TabIndex = 13;
             this.label2.Text = "Password Lama";
             // 
             // btnSimpan
@@ -116,53 +103,68 @@
             this.btnSimpan.AutoSize = true;
             this.btnSimpan.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.btnSimpan.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSimpan.Location = new System.Drawing.Point(219, 183);
-            this.btnSimpan.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSimpan.Location = new System.Drawing.Point(32, 167);
+            this.btnSimpan.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSimpan.Name = "btnSimpan";
-            this.btnSimpan.Size = new System.Drawing.Size(53, 28);
-            this.btnSimpan.TabIndex = 22;
-            this.btnSimpan.Text = "Ubah";
+            this.btnSimpan.Size = new System.Drawing.Size(92, 28);
+            this.btnSimpan.TabIndex = 4;
+            this.btnSimpan.Text = "Rubah (F3)";
             this.btnSimpan.UseVisualStyleBackColor = true;
+            this.btnSimpan.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.Control;
-            this.label1.Location = new System.Drawing.Point(39, 47);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(28, 31);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(48, 18);
-            this.label1.TabIndex = 24;
+            this.label1.TabIndex = 11;
             this.label1.Text = "Nama";
             // 
             // txtNama
             // 
             this.txtNama.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtNama.FormattingEnabled = true;
-            this.txtNama.Location = new System.Drawing.Point(178, 43);
-            this.txtNama.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNama.Location = new System.Drawing.Point(167, 27);
+            this.txtNama.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNama.Name = "txtNama";
             this.txtNama.Size = new System.Drawing.Size(160, 26);
-            this.txtNama.TabIndex = 18;
+            this.txtNama.TabIndex = 0;
             // 
             // txtPass1
             // 
             this.txtPass1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtPass1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPass1.Location = new System.Drawing.Point(178, 76);
-            this.txtPass1.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPass1.Location = new System.Drawing.Point(167, 60);
+            this.txtPass1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPass1.Name = "txtPass1";
             this.txtPass1.PasswordChar = '*';
             this.txtPass1.Size = new System.Drawing.Size(161, 24);
-            this.txtPass1.TabIndex = 19;
+            this.txtPass1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(221, 167);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 28);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Batal (ESC)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // FrmPassword
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.RoyalBlue;
-            this.ClientSize = new System.Drawing.Size(379, 255);
+            this.ClientSize = new System.Drawing.Size(344, 223);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.txtPass3);
@@ -173,8 +175,13 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtNama);
             this.Controls.Add(this.txtPass1);
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FrmPassword";
-            this.Text = "FrmPassword";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Password";
+            this.Load += new System.EventHandler(this.FrmPass_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FrmPass_KeyDown);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -182,7 +189,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txtPass3;
         private System.Windows.Forms.Label label3;
@@ -192,5 +198,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox txtNama;
         private System.Windows.Forms.TextBox txtPass1;
+        private System.Windows.Forms.Button button2;
     }
 }
